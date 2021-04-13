951f21bb854eacd67e20af7776c7e268fe708bf7
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-04-13 20:05:12 -0300

    Header setup

---
+++ wp-content/themes/chustrupfx/header.php
@@ -0,0 +1,8 @@
+<head>
+    <?php wp_head(); ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Victor Inácio é um desenvolvedor web. Habilidades em HTML, CSS, JS, React." />
+    <title>Theme</title>
+</head>
