bf61f12a4f7715e73dec851c52747ebb33d7f5e2
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-04-15 07:47:31 -0300

    script.js registrado em functions.php

--- wp-content/themes/chustrupfx/functions.php
+++ wp-content/themes/chustrupfx/functions.php
@@ -8,7 +8,7 @@ function enqueueScriptsAndStyles() {
 
     $version = wp_get_theme()->get( 'Version' );
     wp_enqueue_style('chustrupfx-style', get_template_directory_uri() . '/style.css', array(), $version);
-
+    wp_enqueue_script('chustrupfx-script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), $version, true);
 
 }
 
